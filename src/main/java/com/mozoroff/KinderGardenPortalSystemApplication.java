2b36e634378d2b5c5e033d865fb7e07d5f46e64d
test <test@example.test>
2022-02-10 10:37:42 +0900

    init

---
+++ src/main/java/com/mozoroff/KinderGardenPortalSystemApplication.java
@@ -0,0 +1,13 @@
+package com.mozoroff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KinderGardenPortalSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KinderGardenPortalSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mozoroff/KinderGardenPortalSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mozoroff;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KinderGardenPortalSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
